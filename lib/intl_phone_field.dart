275d4356f23022b0324816ff5f1a38bbc541f591
Vitaliy Podushkin <plasticfiresam@gmail.com>
2025-07-10 11:19:45 +0700

    feat: added passthrough flag

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -249,6 +249,9 @@ class IntlPhoneField extends StatefulWidget {
   /// If null, default magnification configuration will be used.
   final TextMagnifierConfiguration? magnifierConfiguration;
 
+  /// Enable the search input field in the country picker dialog.
+  final bool enableDialogSearch;
+
   const IntlPhoneField({
     Key? key,
     this.formFieldKey,
@@ -296,6 +299,7 @@ class IntlPhoneField extends StatefulWidget {
     this.pickerDialogStyle,
     this.flagsButtonMargin = EdgeInsets.zero,
     this.magnifierConfiguration,
+    this.enableDialogSearch = true,
   }) : super(key: key);
 
   @override
@@ -373,6 +377,7 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
             widget.onCountryChanged?.call(country);
             setState(() {});
           },
+          enableSearch: widget.enableDialogSearch,
         ),
       ),
     );
